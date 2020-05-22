d47a6de113b1bfd6517796911236c22cde56c473
m03geek <928021-m03geek@users.noreply.gitlab.com>
2020-05-22 12:20:27 +0300

    feat: add some failing stuff

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -5,9 +5,12 @@ describe('name', () => {
     expect(pj.name).toBe('actions-playground');
   });
 });
-
+const unusedVar = 1;
 describe('foo', () => {
   test('js is fine', () => {
     expect(foo(0.1, 0.2)).toBe(0.30000000000000004);
+  });
+  test('failing test', () => {
+    expect(foo(2, 2)).toBe(2);
   });
 });
