0d5bc83c00fb84b20e6d4b6e0a5e28f3424edc86
m03geek <928021-m03geek@users.noreply.gitlab.com>
2020-05-22 12:34:53 +0300

    feat: some actions stuff

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,21 @@
 module.exports = {
   testEnvironment: 'node',
-  // collectCoverage: true,
   verbose: true,
   testMatch: ['**/__tests__/**/*.spec.js'],
   testPathIgnorePatterns: ['/node_modules/', '/build/', '/static/'],
   coverageReporters: ['lcov', 'text', 'json'],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        suiteName: 'jest tests',
+        suiteNameTemplate: '{filepath}',
+        classNameTemplate: '{classname}',
+        titleTemplate: '{title}',
+        outputName: 'junit.xml',
+      },
+    ],
+  ],
   // collectCoverageFrom: ['lib/**/*.js'],
 };
